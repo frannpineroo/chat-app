efb80c4262109fcc285c78f8ee9fc2282020b171
frannpineroo <frannpinero@hotmail.com>
2026-02-10 19:52:41 -0300

    Chat app con express y socketio

---
+++ server/models/usuarios.js
@@ -0,0 +1,15 @@
+class Usuarios {
+
+    constructor() {
+        this.personas = [];
+    }
+
+    agregarPersona( id, nombre ) {
+        let persona = { id, nombre };
+    }
+}
+
+
+module.exports = {
+    Usuarios
+}
