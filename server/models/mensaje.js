d47139bf7eb6413ffd056e3a3c08f13acf82d819
GabyC3 <gabyrc69@gmail.com>
2026-03-01 17:28:51 -0300

    Merge de Rama1 a main

--- server/models/mensaje.js
+++ server/models/mensaje.js
@@ -1,11 +1,21 @@
 const EntityBase = require('./EntityBase');
 const TipoMensaje = require("../shared/Enums/TipoMensaje");
 
-class Mensaje {
-    constructor( info, usuarioId) {
-        this.id = info.id;
-        this.usuarioId = usuarioId;
-    }
+class Mensaje extends EntityBase {
+  constructor({ id, chatId, emisorId, contenido, TipoMensaje = "text", archivo, fechaEnvio = new Date(),
+                archivado = false, leido = false}) 
+    {
+    
+    super({ id });
+    this.chatId = chatId;
+    this.emisorId = emisorId;
+    this.contenido = contenido;
+    this.TipoMensaje = TipoMensaje;
+    this.archivo = archivo instanceof Buffer ? archivo : null;
+    this.fechaEnvio = fechaEnvio;
+    this.archivado = archivado;
+    this.leido = leido;
+  }
 }
 
 module.exports = Mensaje;
